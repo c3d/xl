201d954aad55934b7a382446343bb0e9a220fbe8
sebbrochet <sebbrochet@ed21b5e9-bf16-0410-b9ea-f1dd0686f206>
2004-11-12 19:01:37 +0000

    Some clean-up. This version is now in-sync with the current bootstrap and native XL compilers.
    git-svn-id: https://xlr.svn.sourceforge.net/svnroot/xlr/trunk@92 ed21b5e9-bf16-0410-b9ea-f1dd0686f206

--- xl2/native/make.msvc71.bat
+++ xl2/native/make.msvc71.bat
@@ -1,5 +1,5 @@
 @echo.
-@echo Simple but effective batch to run the basic bootstrapping test under Windows.
+@echo Simple but effective batch to run the basic Native test under Windows.
 @echo To use, run it in the command line with the build config name as the argument.
 @echo After it runs, compare xl2.cpp and xl3.cpp. They must be identical.
 @echo VC 7.1 (.NET 2003) must be ready for command line use (run its vcvars32.bat).
@@ -11,17 +11,19 @@
 
 copy ..\xl.syntax .
 
-..\%_BUILD%\xl2 compiler.xl > xl.cpp
-@if errorlevel 1 goto error
-cl /EHsc /I .. xl.cpp
-@if errorlevel 1 goto error
-
-xl compiler.xl > xl2.cpp
+..\bootstrap\xl compiler.xl > xl2.cpp
 @if errorlevel 1 goto error
 cl /EHsc /I .. xl2.cpp
 @if errorlevel 1 goto error
 
-xl2 compiler.xl > xl3.cpp
+..\native\xl2 -bootstrap compiler.xl > xl3.cpp
+@if errorlevel 1 goto error
+cl /EHsc /I .. xl3.cpp
+@if errorlevel 1 goto error
+
+..\native\xl3 -bootstrap compiler.xl > xl4.cpp
+@if errorlevel 1 goto error
+cl /EHsc /I .. xl4.cpp
 @if errorlevel 1 goto error
 
 @goto done
