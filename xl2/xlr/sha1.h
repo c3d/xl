0e2112b3f50ec02bbd5c7a101a934150d82763bc
Jérôme Forissier <jerome@taodyne.com>
2010-02-03 15:48:38 +0100

    Add SHA-1 functions

---
+++ xl2/xlr/sha1.h
@@ -0,0 +1,60 @@
+#ifndef SHA1_H
+#define SHA1_H
+
+// ****************************************************************************
+//  sha1.h                          (C) XXX
+//                                                            XL2 project
+// ****************************************************************************
+//
+//   File Description:
+//
+//     Function prototypes for the SHA-1 algorithm.
+//
+// Usage example:
+//
+//   char msg[] = "Message to hash";
+//   SHA1_CONTEXT ctx;
+//   byte *hash;
+//
+//   sha1_init(&ctx);
+//   sha1_write(&ctx, (byte *)msg, sizeof(msg)-1);
+//   sha1_final(&ctx);
+//   hash = sha1_read(&ctx);
+//   printf("SHA-1 hash of '%s' is ", msg);
+//   for (int i = 0; i < SHA1_SIZE; i++)
+//     printf("%02x", hash[i]);
+//   printf("\n");
+//
+//
+// ****************************************************************************
+// This program is released under the GNU General Public License.
+// See http://www.gnu.org/copyleft/gpl.html for details
+// ****************************************************************************
+// * File       : $RCSFile$
+// * Revision   : $Revision$
+// * Date       : $Date$
+// ****************************************************************************
+
+#include "base.h"
+
+XL_BEGIN
+
+typedef unsigned int u32;
+
+typedef struct {
+    u32  h0,h1,h2,h3,h4;
+    u32  nblocks;
+    byte buf[64];
+    int  count;
+} SHA1_CONTEXT;
+
+#define SHA1_SIZE 20  /* 20 bytes = 160 bits */
+
+void sha1_init(SHA1_CONTEXT *hd);
+void sha1_write(SHA1_CONTEXT *hd, byte *inbuf, size_t inlen);
+void sha1_final(SHA1_CONTEXT *hd);
+byte *sha1_read(SHA1_CONTEXT *hd);
+
+XL_END
+
+#endif // SHA1_H
