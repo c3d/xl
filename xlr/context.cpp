f84c3d3d36be8d2b97f5b78dea27d5b2b57414f3
Jerome Forissier <jerome@taodyne.com>
2010-12-02 15:39:47 +0100

    Fix GCC 4.4 warning

--- xlr/context.cpp
+++ xlr/context.cpp
@@ -1589,10 +1589,12 @@ Tree *Constraint::SolveFor(Name *name)
             {
                 // Check if we solved it
                 if (Name *n = left->AsName())
+                {
                     if (n->value == name->value)
                         return right;
                     else
                         return NULL;
+                }
 
                 // Check if we have an infix on the left
                 if (Infix *infix = left->AsInfix())
