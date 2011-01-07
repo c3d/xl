3fa06766edbfef09badf3f9dd7f200583f1c476d
Christophe de Dinechin <christophe@taodyne.com>
2011-01-07 08:17:14 +0100

    Disable lazy compilation - Makes debugging annoying

--- xlr/compiler.cpp
+++ xlr/compiler.cpp
@@ -137,7 +137,7 @@ Compiler::Compiler(kstring moduleName, uint optimize_level)
 
     // Select "fast JIT" if optimize level is 0, optimizing JIT otherwise
     runtime = EngineBuilder(module).create();
-    runtime->DisableLazyCompilation(false);
+    runtime->DisableLazyCompilation(true);
 
     // Setup the optimizer - REVISIT: Adjust with optimization level
     optimizer = new FunctionPassManager(module);
