5138e84629789d68451d3eaa3381f61a43d51e0c
Christophe de Dinechin <christophe@taodyne.com>
2011-01-17 09:19:54 +0100

    Removed obsolete ExpressionReduction class

--- xlr/expred.h
+++ xlr/expred.h
@@ -58,40 +58,6 @@ public:
     value_map       computed;     // Values we already computed
 };
 
-
-struct ExpressionReduction
-// ----------------------------------------------------------------------------
-//   Record compilation state for a specific expression reduction
-// ----------------------------------------------------------------------------
-//   Expression reduction is the name given to the process of transforming
-//   a given expression into another tree using the -> operators. For example,
-//   if we have A+B*C -> mla(A,B,C), we will "reduce" 2+3*5 into mla(2,3,5).
-//   This process may involve multiple rewrites if there is some pattern
-//   matching involved, for example if we reduce N! with two rewrite rules,
-//   one for 0!, one for N! when N is not 0.
-{
-
-    ExpressionReduction(CompiledUnit &unit, Tree *source);
-    ~ExpressionReduction();
-
-    void                NewForm();
-    void                Succeeded();
-    void                Failed();
-
-public:
-    CompiledUnit &      unit;           // Compilation unit we use
-    Tree *              source;         // Tree we build (mostly for debugging)
-    llvm::LLVMContext * llvm;           // Inherited LLVM context
-
-    llvm::BasicBlock *  savedfailbb;    // Saved location of failbb
-
-    llvm::BasicBlock *  entrybb;        // Entry point to subcase
-    llvm::BasicBlock *  savedbb;        // Saved position before subcase
-    llvm::BasicBlock *  successbb;      // Successful completion of expression
-
-    value_map           savedvalue;     // Saved compile unit value map
-};
-
 XL_END
 
 #endif // COMPILER_EXPRED_H
