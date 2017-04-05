eb438081a92539fe4c55c2af05261f6e3f60fd93
Christophe de Dinechin <dinechin@redhat.com>
2017-04-05 07:36:33 +0200

    Ensure we dont include windows.h in regular code
    This defines things like CONTEXT or ERROR which conflict with the rest
    of the code.

--- src/utf8_fileutils.h
+++ src/utf8_fileutils.h
@@ -45,26 +45,9 @@
 
 #ifdef CONFIG_MINGW
 
-#include <ext/stdio_filebuf.h>
-#include <windows.h>
-#include <fcntl.h>
-
 typedef struct _stat utf8_filestat_t;
 
-
-inline std::wstring utf8_decode(const std::string &str)
-// ----------------------------------------------------------------------------
-//    Convert a UTF-8 std::string into a UTF-16 std::wstring
-// ----------------------------------------------------------------------------
-{
-    int needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(),
-                                     NULL, 0);
-    std::wstring dest(needed, 0);
-    MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(),
-                        &dest[0], needed);
-    return dest;
-}
-
+std::wstring utf8_decode(const std::string &str);
 
 class utf8_ifstream : public std::istream
 // ----------------------------------------------------------------------------
@@ -80,76 +63,20 @@ public:
     utf8_ifstream()
         : istream_type()
     {}
-
-    explicit
-    utf8_ifstream(const char* s,
-                  std::ios::openmode mode = std::ios::in)
-        : istream_type(&b),
-          b(_wopen(utf8_decode(s).c_str(), wflags(mode)), mode)
-    {}
+    explicit utf8_ifstream(const char* s,
+                           std::ios::openmode mode = std::ios::in);
 
 protected:
-    int wflags(std::ios::openmode)
-    {
-        int flags = _O_RDONLY;
-        // Experience shows that some versions of GCC misbehave on input.unget()
-        // if binary mode is not set, see #2682
-        flags |= _O_BINARY;
-        return flags;
-    }
+    int wflags(std::ios::openmode);
 
 protected:
     filebuf_type b;
 };
 
 
-inline time_t fileTimeToTime_t(FILETIME ft)
-// ----------------------------------------------------------------------------
-//   Conversion to more portable file time
-// ----------------------------------------------------------------------------
-{
-   ULARGE_INTEGER ull;
-   ull.LowPart = ft.dwLowDateTime;
-   ull.HighPart = ft.dwHighDateTime;
-   return ull.QuadPart / 10000000ULL - 11644473600ULL;
-}
-
-
-inline int utf8_stat(const char *path, struct _stat *buffer)
-// ----------------------------------------------------------------------------
-//   Stat function taking a UTF-8 path
-// ----------------------------------------------------------------------------
-{
-    std::wstring wpath = utf8_decode(std::string(path));
-    int status = _wstat(wpath.c_str(), buffer);
-    if (status < 0)
-        return status;
-    
-    // Bug#1567: _wsat returns bogus file times when daylight saving
-    // time setting changes
-    // By using GetFileAttributesEx we are consistent with
-    // QFileInfo::lastModified().toTime_t().
-    WIN32_FILE_ATTRIBUTE_DATA attr;
-    bool ok = GetFileAttributesEx((const wchar_t *)wpath.c_str(),
-                                  GetFileExInfoStandard, &attr);
-    if (ok)
-    {
-        buffer->st_ctime = fileTimeToTime_t(attr.ftCreationTime);
-        buffer->st_atime = fileTimeToTime_t(attr.ftLastAccessTime);
-        buffer->st_mtime = fileTimeToTime_t(attr.ftLastWriteTime);
-    }
-    return 0;
-}
-
-
-inline int utf8_access(const char *path, int mode)
-// ----------------------------------------------------------------------------
-//   Access function taking a UTF-8 path
-// ----------------------------------------------------------------------------
-{
-    std::wstring wpath = utf8_decode(std::string(path));
-    return _waccess(wpath.c_str(), mode);
-}
+extern time_t   fileTimeToTime_t(FILETIME ft);
+extern int      utf8_stat(const char *path, struct _stat *buffer);
+extern int      utf8_access(const char *path, int mode);
 
 
 inline bool isDirectorySeparator(int c)
