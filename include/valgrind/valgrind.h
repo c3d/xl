b5ae3b893570894f65a3e05c06a7417fa88b8cea
Christophe de Dinechin <christophe@dinechin.org>
2019-03-28 22:53:54 +0100

    Copyright typo fix

--- include/valgrind/valgrind.h
+++ include/valgrind/valgrind.h
@@ -18,7 +18,7 @@
 // *****************************************************************************
 // This file is part of XL
 //
-// XL is free software: you can r redistribute it and/or modify
+// XL is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
