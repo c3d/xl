7ae2d2ec55f4f1d099e5fb1c80c3dc2ad2470488
Christophe de Dinechin <christophe@dinechin.org>
2020-06-23 19:14:27 +0200

    Change comment to describe intent for new symbol table structure
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

--- include/context.h
+++ include/context.h
@@ -12,13 +12,14 @@
 //     keep symbolic information, e.g. how to rewrite trees, and the
 //     runtime environment (Runtime), which we use while executing trees
 //
-//
+//     That execution context is stored in an XL scope structure,
+//     see http://c3d.github.io/xl/#scopes-and-maps.
 //
 //
 // *****************************************************************************
 // This software is licensed under the GNU General Public License v3+
 // (C) 2012, Catherine Burvelle <catherine@taodyne.com>
-// (C) 2003-2004,2009-2012,2014-2020, Christophe de Dinechin <christophe@dinechin.org>
+// (C) 2003-2020, Christophe de Dinechin <christophe@dinechin.org>
 // (C) 2010-2011,2013, Jérôme Forissier <jerome@taodyne.com>
 // *****************************************************************************
 // This file is part of XL
@@ -38,141 +39,33 @@
 // If not, see <https://www.gnu.org/licenses/>.
 // *****************************************************************************
 /*
-  COMPILATION STRATEGY:
+  SYMBOL TABLE:
 
-  The version of XL implemented here is a very simple language based
-  on tree rewrites, designed to serve as a dynamic document description
-  language (DDD), as well as a tool to implement the "larger" XL in a more
-  dynamic way. Both usage models imply that the language is compiled on the
-  fly, not statically. We use LLVM as a back-end, see compiler.h.
+  A symbol table is represented as a prefix sequence of contexts:
 
-  Also, because the language is designed to manipulate program trees, which
-  serve as the primary data structure, this implies that the program trees
-  will exist at run-time as well. As a resut, there needs to be a
-  garbage collection phase. The chosen garbage collection technique is
-  based on reference counting because trees are not cyclic, so that
-  ref-counting is both faster and simpler.
+     {
+         X is 1
+         Y is 2
+     } {
+         Z is 3
+         T is "Hello"
+     }
 
+  The left of the prefix is the parent context.
+  The right of the prefix is the current context.
+  This prefix structure is typedefed as a "Scopes" (plural).
+  Each of the levels is a Block surrounded by { }, and is
+  typedefed as "Scope" (singular).
 
-  PREDEFINED FORMS:
+  Each of the levels contains either
 
-  XL is really built on a very small number of predefined forms recognized by
-  the compilation phase.
+  - An empty name if there is no definition in the scope (empty scope)
+  - One Rewrite, which is an Infix "is" or ":="
+  - One Rewrites sequence of rewrites, which is an infix "\n"
 
-    "A->B" defines a rewrite rule, rewriting A as B. The form A can be
-           "guarded" by a "when" clause, e.g. "N! when N>0 -> N * (N-1)!"
-    "A:B" is a type annotation, indicating that the type of A is B
-    "(A)" is the same as A, allowing to override default precedence,
-          and the same holds for A in an indentation (indent block)
-    "A;B" is a sequence, evaluating A, then B, the value being B.
-          The newline infix operator plays the same role.
-    "data A" declares A as a form that cannot be reduced further.
-          This can be used to declare data structures.
-
-  The XL type system is itself based on tree shapes. For example,
-  "integer" is a type that covers all Integer trees. Verifying if X
-  has type Y is performed by evaluting the value X:Y.
-
-  - In the direct type match case, this evaluates to X.
-    For example, '1:integer' will evaluate directly to 1.
-
-  - The X:Y expression may perform an implicit type conversion.
-    For example, '1:real' will evaluate to 1.0 (converting 1 to real)
-
-  - Finally, in case of type mismatch, X:Y evaluates to the same X:Y tree.
-
-  The compiler is allowed to special-case any such form. For example, if it
-  determines that the type of a tree is "integer", it may represent it using a
-  machine integer. It must however convert to/from tree when connecting to
-  code that deals with less specialized trees.
-
-  The compiler is also primed with a number of declarations such as:
-     x:integer+y:integer -> [builtin]
-  This tells the compiler to lookup a native compilation function. These
-  functions are declared in basics.tbl (and possibly additional .tbl files)
-
-
-  RUNTIME EXECUTION:
-
-  The evaluation functions pointed to by 'code' in the Tree structure are
-  designed to be invoked by normal C code using the normal C stack. This
-  facilitate the interaction with other code.
-
-  At top-level, the compiler generates only functions with the same
-  prototype as native_fn, i.e. Tree * (Context *, Tree *). The code is being
-  generated on invokation of a form, and helps rewriting it, although
-  attempts are made to leverage existing rewrites.
-
-  Compiling such top-level forms invokes a number of rewrites. A
-  specific rewrite can invoke multiple candidates. For example,
-  consider the following factorial declaration:
-     0! -> 1
-     N! where N>0 -> N * (N-1)!
-
-  In that case, code invoking N! will generate a test to check if N is 0, and
-  if so invoke the first rule, otherwise invoke the second rule. The same
-  principle applies when there are guarded rules, i.e. the "when N>0" clause
-  will cause a test N>0 to be added guarding the second rule.
-
-  If all these invokations fail, then the input tree is in "reduced form",
-  i.e. it cannot be reduced further. If it is a non-leaf tree, then an attempt
-  is made to evaluate its children.
-
-  If no evaluation can be found for a given form that doesn't match a 'data'
-  clause, the compiler will emit a diagnostic. This is not a fatal condition,
-  however, and code will be generated that simply leaves the tree as is when
-  that happens.
-
-
-  GENERATING CODE FOR REWRITES:
-
-  The code for a rewrite is kept in the 'code' field of the definition.
-  This definition should never be evaluated directly, because the code field
-  doesn't have the expected signature for evaluation. Specifically,
-  it has additional Tree * parameters, corresponding to the variables
-  in the pattern (the left-hand side of the rewrite).
-
-  For example, the rewrite 'bar X:integer, Y -> foo X, Y+1' has two variables
-  in its pattern, X and Y. Therefore, the code field for 'foo X, Y+1' will
-  have the following signature: Tree *(Tree *self, Tree *X, Tree *Y).
-  Note that bar is not a variable for the pattern. While the name 'bar' is
-  being defined partially by this rewrite, it is not a variable in the pattern.
-
-  The generated code will only be invoked if the conditions for invoking it
-  are fulfilled. In the example, it will not be invoked if X is not an integer
-  or for a form such as 'bar 3' because 3 doesn't match 'X:integer, Y'.
-
-
-  CLOSURES:
-
-  Closures are a way to "embed" the value of variables in a tree so that
-  it can be safely passed around. For example, consider
-      AtoB T ->
-         A; do T; B
-      foo X ->
-         AtoB { write X+1 }
-
-  In this example, the rules of the language state that 'write X+1' is not
-  evaluated before being passed to 'AtoB', because nothing in the 'AtoB'
-  pattern requires evaluation. However, 'write X+1' needs the value of 'X'
-  to evaluate properly in 'AtoB'. Therefore, what is passed to 'AtoB' is
-  a closure retaining the value of X in 'foo'.
-
-  In the symbol table, closures are simply implemented as additional
-  declarations that precede the original code. For example, in teh
-  above example, if X is 17 in the environment, the closure would be
-  created as a scope containing { X -> 17 }, and would be applied as a
-  prefix, i.e. { X -> 17 } { write X+1 }.
-
-  In boxed formats, closures take a slightly more complicated form.
-
-
-  LAZY EVALUATION:
-
-  Trees are evaluated as late as possible (lazy evaluation). In order to
-  achieve that, the compiler maintains a boolean variable per tree that may
-  be evaluated lazily. This variable is allocated by CompilerUnit::NeedLazy.
-  It is set when the value is evaluated, and initially cleared.
+  In order to keep lookup O(log(N)) instead of O(N), the Infix ; are
+  reordered based on a hash function, so that you may go left or right
+  at each stage based on the lowest bit of the hash function.
  */
 
 #include "base.h"
@@ -192,23 +85,32 @@ XL_BEGIN
 //    Forward type declarations
 //
 // ============================================================================
+//  The following express XL types that are not easy to express in C++:
+//     scope: A prefix where left is nil or a block and right is a block
+//     scopemap: a {} block containing onlyi rewrites
+//     rewrite: An infix that is 'A is B' or 'A := B'
+//     rewrites: An infix that is 'A \n B' or 'A ; B'
 
 struct Context;                                 // Execution context
 
 // Give names to components of a symbol table
 typedef Prefix                          Scope;
 typedef GCPtr<Scope>                    Scope_p;
+typedef Block                           ScopeMap;
+typedef GCPtr<ScopeMap>                 ScopeMap_p;
+typedef Infix                           Rewrites;
+typedef GCPtr<Rewrites>                 Rewrites_p;
 typedef Infix                           Rewrite;
 typedef GCPtr<Rewrite>                  Rewrite_p;
 typedef Infix                           RewriteChildren;
 typedef GCPtr<RewriteChildren>          RewriteChildren_p;
 
+// API used to access a context
 typedef GCPtr<Context>                  Context_p;
 typedef std::vector<Infix_p>            RewriteList;
 typedef std::map<Tree_p, Tree_p>        tree_map;
 typedef Tree *                          (*eval_fn) (Scope *, Tree *);
 typedef std::map<Tree_p, eval_fn>       code_map;
-
 
 
 // ============================================================================
